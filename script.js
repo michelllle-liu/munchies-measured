06db04e19682b43477b4ec0739f80521c8063b8e
Michelle <77900309+michelllle-liu@users.noreply.github.com>
2021-04-10 00:24:02 -0400

    Update script.js
    js for modal box and recommended calorie intake

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+// DETERMINING THE RECOMMENDED DAILY CALORIE INTAKE
+
+let modal = document.getElementById("myModal");
+
+let link = document.getElementById("modalLink");
+
+link.onclick = function() {
+  modal.style.display = "block";
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const myForm= document.getElementById("CalorieIntake"); 
+myForm.addEventListener('submit', submit);
+
+function submit(event){
+  event.preventDefault();
+	
+  const formData = new FormData(myForm);//get form data
+  const person = Object.fromEntries(formData);//convert form data to object
+  
+  console.log(person.weight);
+  console.log(person.heightFT);
+  console.log(person.heightInch);
+  console.log(person.age);
+  console.log(person.sex);
+  console.log(person.activity);
+
+ 
+  CalculateDailyIntake(person);
+  myForm.reset();
+}
+
+function CalculateDailyIntake(person){
+  let heightCM=(person.heightFT*30.48)+ (person.heightInch*2.54);
+
+  let weightKG= person.weight/2.20462262;
+  console.log(weightKG);
+
+	let factor=0.0
+  if(person.activity==="Sedentary"){
+    factor=1.2;}
+
+  if(person.activity==="LowModerate"){
+    factor=1.4;}
+    
+  if(person.activity==="VeryActive"){
+    factor=1.6;}
+
+  let BMR=0.0
+  let dailyIntake=0;
+  
+  if(person.sex==="Male"){
+    BMR=(10.0*weightKG) + (6.25*heightCM) - (5*person.age) + 5.0;
+    console.log(BMR);
+
+    dailyIntake= Math.round(BMR * factor);
+    console.log(`daily calorie intake ${dailyIntake} calories`);
+  }
+  else{
+    BMR=(10.0*weightKG) + (6.25*heightCM) - (5*person.age) -161.0;
+    console.log(BMR);
+
+    dailyIntake= Math.round(BMR * factor);
+    console.log(`daily calorie intake ${dailyIntake} calories`);
+  }
+
+  document.getElementById('recCal').value=`${dailyIntake}`;
+
+	modal.style.display = "none";
+}
+
 // SCRIPT RELATING TO THE API
 
 async function foodSearch (foodName) {
